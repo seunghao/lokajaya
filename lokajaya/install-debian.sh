3d1257d26fad2c5e095f32ed3bd95394df81e2d8
seunghao <146314741+seunghao@users.noreply.github.com>
2024-03-16 02:42:03 +0800

    Add files via upload

---
+++ lokajaya/install-debian.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+# Update packages list and update system
+apt update
+apt upgrade -y
+
+# Install nala
+apt install nala -y
+
+# Making .config and Moving config files and background to Pictures
+cd $builddir
+mkdir -p /home/$username/.config
+mkdir -p /home/$username/.fonts
+mkdir -p /home/$username/Pictures
+mkdir -p /home/$username/Pictures/backgrounds
+cp -R dotconfig/* /home/$username/.config/
+cp bg.jpg /home/$username/Pictures/backgrounds/
+mv user-dirs.dirs /home/$username/.config
+chown -R $username:$username /home/$username
+
+# Installing Essential Programs 
+nala install feh kitty rofi picom thunar nitrogen lxpolkit x11-xserver-utils unzip wget pipewire wireplumber pavucontrol build-essential libx11-dev libxft-dev libxinerama-dev libx11-xcb-dev libxcb-res0-dev zoxide -y
+# Installing Other less important Programs
+nala install neofetch flameshot psmisc mangohud vim lxappearance papirus-icon-theme lxappearance fonts-noto-color-emoji lightdm -y
+
+# Download Nordic Theme
+cd /usr/share/themes/
+git clone https://github.com/EliverLara/Nordic.git
+
+# Installing fonts
+cd $builddir 
+nala install fonts-font-awesome -y
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip -d /home/$username/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip -d /home/$username/.fonts
+mv dotfonts/fontawesome/otfs/*.otf /home/$username/.fonts/
+chown $username:$username /home/$username/.fonts/*
+
+# Reloading Font
+fc-cache -vf
+# Removing zip Files
+rm ./FiraCode.zip ./Meslo.zip
+
+# Install Nordzy cursor
+git clone https://github.com/alvatip/Nordzy-cursors
+cd Nordzy-cursors
+./install.sh
+cd $builddir
+rm -rf Nordzy-cursors
+
+# Install floorp-browser
+nala install apt-transport-https curl -y
+curl -fsSL https://ppa.ablaze.one/KEY.gpg | gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
+curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.ablaze.one/Floorp.list'
+nala update
+nala install floorp -y
+
+# Enable graphical login and change target from CLI to GUI
+systemctl enable lightdm
+systemctl set-default graphical.target
+
+# Enable wireplumber audio service
+
+sudo -u $username systemctl --user enable wireplumber.service
+
+# Beautiful bash
+git clone https://github.com/ChrisTitusTech/mybash
+cd mybash
+bash setup.sh
+cd $builddir
+
+# DWM Setup
+git clone https://github.com/ChrisTitusTech/dwm-titus
+cd dwm-titus
+make clean install
+cp dwm.desktop /usr/share/xsessions
+cd $builddir
+
+# Use nala
+bash scripts/usenala
